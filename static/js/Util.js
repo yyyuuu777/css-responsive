0136f7723c9f3ab8828ed8d45339969224d49145
jash <jash@moonjoy.net>
2017-08-28 17:48:47 +0700

    update

--- static/js/Util.js
+++ static/js/Util.js
@@ -2,49 +2,76 @@
  * Created by ly on 8/27/2017 AD.
  */
 
-(function(){
-
-   var dialog = {
-       dialogId :'dialog',
-   }
+(function () {
 
     var Util = function () {
+        this.head = "dialog-head";
+    }
+    /**
+     *  {param : function} sure btn : callBack
+     */
+    Util.prototype.initDialog = function (id, callBack) {
+        this.addBg();
+        this.dialogId = id;
+        this.dom_dialog = this.getDom(this.dialogId);
+        this.addCloseBtn(this.dom_dialog.children[0]);
+        this.callBack = callBack;
+        if (this.dom_dialog.children[2]) {
+            this.addEve([
+                //todo:// change dome get way
+                this.dom_dialog.children[2].children[0],
+                this.dom_dialog.children[2].children[1]
+            ]);
+        }
+        return this;
     }
 
-    Util.prototype.initDialog= function () {
+    Util.prototype.addBg = function () {
         var dom = document.createElement("div");
         var style = "display:none;position:fixed;top:0;bottom:0;left:0;right:0;width:100%;height:100%;background-color: rgba(0,0,0,0.5);z-index:10"
-        dom.style.cssText= style;
-        document.getElementsByTagName("body")[0].appendChild(dom);
-        var dom_dialog = this.dom_dialog = this.getDom(dialog.dialogId);
-        dom_dialog.style.position = "fixed";
-        dom_dialog.style.top = 0;
-        dom_dialog.style.bottom = 0;
-        dom_dialog.style.left = 0;
-        dom_dialog.style.right = 0;
-        dom_dialog.style.maxHeight= "500px";
-        dom_dialog.style.maxWidth= "500px";
-        dom_dialog.style.minWidth = '200px';
-        dom_dialog.style.minHeight = '200px';
-        dom_dialog.style.background = '#fff';
-        dom_dialog.style.display="none";
-        dom_dialog.style.margin = "auto";
+        dom.style.cssText = style;
         this.dialog_bg = dom;
+        document.getElementsByTagName("body")[0].appendChild(dom);
     }
 
-    Util.prototype.show_dialog = function(){
-       this.dialog_bg.style.display = 'block'  ;
-       this.dom_dialog.style.display = 'block';
+    Util.prototype.addCloseBtn = function (dom) {
+        dom.appendChild(this.createCloseBtn());
     }
 
-    Util.prototype.hide_dialog = function(){
-        var dialog = this.getDom(dialog.dialogId);
-        this.dialog_bg.style.display = 'none'  ;
+    Util.prototype.createCloseBtn = function () {
+        var closeBtn = document.createElement("p");
+        var imgBtn = document.createElement("img");
+        imgBtn.setAttribute("src", "./static/imgs/close.png");
+        closeBtn.setAttribute("id", "close");
+        closeBtn.appendChild(imgBtn);
+        this.addEve([closeBtn]);
+        return closeBtn;
+    }
+    Util.prototype.addEve = function (arr) {
+        arr.map((item) => {
+            // get all event
+            item.addEventListener('click', () => {
+                    item.className.indexOf("sure") >= 0 && this.callBack();
+                    this.hide_dialog();
+                }
+            )
+        })
+    }
+
+    Util.prototype.show_dialog = function () {
+        this.dialog_bg.style.display = 'block';
+        this.dom_dialog.style.display = 'block';
+    }
+
+    Util.prototype.hide_dialog = function () {
+        this.dialog_bg.style.display = 'none';
         this.dom_dialog.style.display = 'none';
     }
-
     Util.prototype.getDom = function (id) {
         return document.getElementById(id);
     }
-    window.util = new Util();
+    Util.prototype.getDomsByClass = function (className) {
+        return document.getElementsByClassName(className);
+    }
+    window.util = Util;
 })();
